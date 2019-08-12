d50dbf654d4c7f3fc5b05fb0e0f919c76c599269
Etheon <etheanore@gmail.com>
2019-08-12 18:20:25 +0200

    test

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello")
+const container = document.querySelector("#container");
+let newDiv = document.createElement("div");
+newDiv.setAttribute("id", "block");
+
+for (let i = 0; i < 256; i++){
+    container.appendChild(newDiv)
+}
