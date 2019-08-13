dd7545682016acf0fd21a403d10e8825bb942132
Etheon <etheanore@gmail.com>
2019-08-13 18:06:03 +0200

    First result of the etch-a-sketch (clear button + resize)

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
+// Instanciate the first grid (size -> 36)
 const container = document.querySelector("#container");
-let newDiv = document.createElement("div");
-newDiv.setAttribute("id", "block");
+const maxSize = parseInt(window.getComputedStyle(container, null).getPropertyValue("width"));
+console.log(maxSize)
+const clearButton = document.querySelector("#clearButton");
+createGrid();
+clearButton.addEventListener("click", reset);
 
-for (let i = 0; i < 256; i++){
-    container.appendChild(newDiv)
+function changeBackgroundColor(){
+    // Change the color of a block background
+    this.setAttribute("class", "hoverBlock");
 }
+
+function reset(){
+    // Reset the grid and ask the user for a new size
+    const blocks = document.querySelectorAll("#block")
+    blocks.forEach(block => {
+        block.remove();
+    });
+    gridNumber = window.prompt("Taille de votre grille");
+    createGrid(gridNumber)
+    
+}
+
+function createGrid(x = 36){
+    // Create the grid. 
+    container.style.gridTemplateColumns = `repeat(${x}, minmax(${maxSize/x}px, 0fr))`;
+    for (let i = 0; i < (x * x); i++){
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "block");
+        container.appendChild(newDiv)
+    }
+    const blocks = document.querySelectorAll("#block")
+    blocks.forEach(block => {
+        block.addEventListener("mouseover", changeBackgroundColor)
+        block.style.width = `${maxSize/x}px`
+    block.style.height = `${maxSize/x}px`
+    });
+    
+}
+
+
+
+
+
